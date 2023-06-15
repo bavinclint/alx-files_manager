ea1e0eaf23bd71683f56d4dd79ea907daa37e08b
bavinclint <bavinclint@gmail.com>
2023-06-15 12:43:37 +0300

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,43 @@
-// eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
-import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
-import UsersController from '../controllers/UsersController';
-import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
+/* eslint-disable */
+import { Router } from "express";
+import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
+import FilesController from "../controllers/FilesController";
 
-/**
- * Injects routes with their handlers to the given Express application.
- * @param {Express} api
- */
-const injectRoutes = (api) => {
-  api.get('/status', AppController.getStatus);
-  api.get('/stats', AppController.getStats);
+// Creating a new router instance
+const router = Router();
 
-  api.get('/connect', basicAuthenticate, AuthController.getConnect);
-  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+// Defining a GET route for "/status" and mapping it to the getStatus method in the AppController
+router.get("/status", AppController.getStatus);
 
-  api.post('/users', UsersController.postNew);
-  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+// Defining a GET route for "/stats" and mapping it to the getStats method in the AppController
+router.get("/stats", AppController.getStats);
 
-  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
-  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
-  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
-  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
-  api.get('/files/:id/data', FilesController.getFile);
+// Defining a POST route for "/users" and mapping it to the postNew method in the UsersController
+router.post("/users", UsersController.postNew);
 
-  api.all('*', (req, res, next) => {
-    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
-  });
-  api.use(errorResponse);
-};
+// Defining a GET route for "/connect" and mapping it to the getConnect method in the AuthController
+router.get("/connect", AuthController.getConnect);
 
-export default injectRoutes;
+// Defining a GET route for "/disconnect" and mapping it to the getDisconnect method in the AuthController
+router.get("/disconnect", AuthController.getDisconnect);
+
+// Defining a GET route for "/users/me" and mapping it to the getMe method in the UsersController
+router.get("/users/me", UsersController.getMe);
+
+// Defining a POST route for "/files" and mapping it to the postUpload method in the FilesController
+router.post("/files", FilesController.postUpload);
+
+router.get("/files/:id", FilesController.getShow);
+
+router.get("/files", FilesController.getIndex);
+
+router.put("/files/:id/publish", FilesController.putPublish);
+
+router.put("/files/:id/unpublish", FilesController.putUnpublish);
+
+router.get("/files/:id/data", FilesController.getFile);
+
+// Exporting the router module
+module.exports = router;
